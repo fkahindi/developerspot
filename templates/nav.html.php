299a072cd828077f199c9b8744f83e4323a729af
Francis <fkahindi@gmail.com>
2020-02-08 21:32:29 +0300

    Add about page

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -3,5 +3,5 @@
 	<li><a href="<?php echo BASE_URL ?>templates/topic_page.html.php?name=html"> Html</a></li> <span>&#124;</span>
 	<li><a href="<?php echo BASE_URL ?>templates/topic_page.html.php?name=javascript"> JavaScript</a></li><span>&#124;</span>
 	<li><a href="<?php echo BASE_URL ?>templates/topic_page.html.php?name=php"> PHP</a></li> <span>&#124;</span>
-	<li><a href="#"> About</a></li> 
+	<li><a href="<?php echo BASE_URL ?>about.php"> About</a></li> 
 </ul>
