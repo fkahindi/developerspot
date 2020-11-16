221052db8e6a1a4958cbf421000e2302e3343ee4
Francis <fkahindi@gmail.com>
2020-11-16 08:09:37 +0300

    Change Close button from &times to a close entity code

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -1,5 +1,5 @@
 <ul> 
-	<a href="javascript:void(0)" class="closebtn">&times;</a>
+	<a href="javascript:void(0)" class="closebtn"> &#10006;</a>
 	<li><a href="<?php echo BASE_URL ?>index.php">Home</a></li> <span>&#124;</span>
 	<li><a href="<?php echo BASE_URL ?>templates/topic_page.html.php?name=html"> Html</a></li> <span>&#124;</span>
 	<li><a href="<?php echo BASE_URL ?>templates/topic_page.html.php?name=javascript"> JavaScript</a></li><span>&#124;</span>
