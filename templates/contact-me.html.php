1f66067d47de39ced4b2d58844d984f15121bb34
Francis <fkahindi@gmail.com>
2020-12-05 09:17:30 +0300

    Move contact me profile images to contact-me folder.

--- templates/contact-me.html.php
+++ templates/contact-me.html.php
@@ -1,3 +1,11 @@
-<img src="<?php echo BASE_URL ?>resources/photos/franciskahindi-01.jpg" alt="Francis-photo" loading="lazy" width="50px" height="60px">
+<picture>
+    <source type="image/webp" media="(max-width: 599px)" srcset="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-sm.webp 1x, <?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-sm@2x.webp 2x">
+    <source type="image/webp" media="(max-width: 1199px)" srcset="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-md.webp 1x, <?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-md@2x.webp 2x">
+    <source type="image/webp" media="(min-width: 1200px)" srcset="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-lg.webp 1x, <?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-lg@2x.webp 2x">
+    <source type="image/jpg" media="(max-width: 599px)" srcset="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-sm.jpg 1x, <?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-sm@2x.jpg 2x">
+    <source type="image/jpg" media="(max-width: 1199px)" srcset="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-md.jpg 1x, <?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-md@2x.jpg 2x">
+    <source type="image/jpg" media="(min-width: 1200px)" srcset="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-lg.jpg 1x, <?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01-lg@2x.jpg 2x">    
+    <img src="<?php echo BASE_URL ?>resources/photos/contact-me/franciskahindi-01.jpg" alt="profile-image" loading="lazy" width="50px" height="60px" />
+</picture>
 <p>Hi, I'm <em><strong>Francis Kahindi</strong></em>.<br> I help build fast, responsive, SEO friendly websites and I'm free for freelance work. 
 <button> <a href="<?php echo BASE_URL ?>templates/contact-me-form.html.php">Contact Me</a></button> </p>
