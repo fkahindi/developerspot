a9ede2549209d67cef6accd5171707a4907644a5
Francis <fkahindi@gmail.com>
2021-04-14 10:26:37 +0300

    Reverse policies links deletion

--- templates/footer.html.php
+++ templates/footer.html.php
@@ -3,9 +3,9 @@
 	<?php include  __DIR__ . '/nav.html.php'; ?>	
 </div>
 <div class="policies">
-	<p><a href="<?php echo BASE_URL ?>/policies/privacy-policy.php">Privacy policy</a></p>
-	<p><a href="<?php echo BASE_URL ?>/policies/terms-conditions.php">Terms & Conditions </a></p>
-	<p><a href="<?php echo BASE_URL ?>/policies/cookie-policy.php">Cookie policy</a></p>
+	<p><a href="<?php echo BASE_URL ?>policies/privacy-policy.php">Privacy policy</a></p>
+	<p><a href="<?php echo BASE_URL ?>policies/terms-conditions.php">Terms & Conditions </a></p>
+	<p><a href="<?php echo BASE_URL ?>policies/cookie-policy.php">Cookie policy</a></p>
 </div>
 <div class="copyright">
 	<div>
