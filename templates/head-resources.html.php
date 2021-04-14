ffe2b5a55b668151b315f2accc6096ad8f49affe
Francis <fkahindi@gmail.com>
2021-04-14 10:25:15 +0300

    Link css font-awesome-4.7.0 for local server only

--- templates/head-resources.html.php
+++ templates/head-resources.html.php
@@ -1,9 +1,9 @@
 <link rel="preload" href="<?php echo BASE_URL ?>resources/css/main.css" as="style">
-<link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" as="style">
+<link rel="preload" href="<?php echo BASE_URL ?>resources/font-awesome-4.7.0/css/font-awesome.min.css" as="style">
 <link rel="preload" href="<?php echo BASE_URL ?>resources/js/page-control.js" as="script">
 <link rel="preload" href="<?php echo BASE_URL ?>resources/fonts/Aladin-Regular.ttf" as="font" type="truetype" crossorigin>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/subscribe-comment-form.css" media="print" onload="this.media='all'; this.onload=null;"/>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/main.css" />
 <link rel="icon" href="<?php echo BASE_URL ?>resources/icons/devpot-favicon.png" sizes="16x16 32x32" type="image/x-icon"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" media="print" onload="this.media='all'; this.onload=null;"/>
+<link rel="stylesheet" href="<?php echo BASE_URL ?>resources/font-awesome-4.7.0/css/font-awesome.min.css" media="print" onload="this.media='all'; this.onload=null;"/>
 
