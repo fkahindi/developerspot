c419addd73ad13e6bd55384f2b1b889e42858d39
Francis <fkahindi@gmail.com>
2021-04-28 19:20:36 +0300

    Redesign social icons

--- templates/social-icons-links.php
+++ templates/social-icons-links.php
@@ -10,15 +10,14 @@ $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 //$query = $_SERVER['QUERY_STRING'];
 //echo $query; // Outputs: Query String
 ?>
+<div class="social-media">
 <!--Facebook button-->
 <a target="_blank" class="fa fa-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url;?>"  data-layout="button" data-href="<?php echo $url;?>" ></a>
  <!--Twitter button-->
-<a target="_blank" class="fa fa-twitter" href="https://twitter.com/share?url=<?php echo $url;?>&amp;via=developerspotKE&amp;text=<?php echo $posts['post_title'] ?>&amp;hashtags=webdevelopment,webdesign" >
-</a>
-<!--Linkedin button-->
-<span><a target="_blank" class="fa fa-linkedin" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $url;?>&title=<?php echo $posts['post_title'] ?>&source=developerspot.co.ke" ></a></span>
+<a target="_blank" class="fa fa-twitter" href="https://twitter.com/share?url=<?php echo $url;?>&amp;via=developerspotKE&amp;text=<?php echo $posts['post_title'] ?>&amp;hashtags=webdevelopment,webdesign" ></a>
 <!--Pinterest button-->
-<span><a target="_blank" class="fa fa-pinterest" href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonPin" data-pin-save="true" data-pin-custom="true"></a></span>
-<!--WhataApp button-->
-<span><a target="_blank" class="fa fa-whatsapp"
-href="https://api.whatsapp.com/send?&text=<?php echo $posts['post_title'] ?> <?php echo $url;?>" data-action="share/whatsapp/share"></a></span>
+<a target="_blank" class="fa fa-pinterest" href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonPin" data-pin-save="true" data-pin-custom="true"></a>
+<!--instagram button-->
+<a target="_blank" class="fa fa-instagram" href="=<?php echo $url;?>&title=<?php echo $posts['post_title'] ?>&source=developerspot.co.ke" ></a>
+
+</div>
