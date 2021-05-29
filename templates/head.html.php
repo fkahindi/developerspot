cfc2f8eae73e4f3ff898bca0423fc05f2ce727c7
Francis <fkahindi@gmail.com>
2021-05-29 14:20:19 +0300

    Add page url ouput function

--- templates/head.html.php
+++ templates/head.html.php
@@ -1,3 +1,12 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+//echo $uri; // Outputs: URI
+ 
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+ 
+$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+ // Outputs: Full URL
+ ?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160763224-1"></script>
 <script>
