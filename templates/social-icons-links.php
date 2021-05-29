ca7b8904a9eb971d5bb0a0dd873e702319a9e1ed
Francis <fkahindi@gmail.com>
2021-05-29 14:30:07 +0300

    Make changes to social links urls

--- templates/social-icons-links.php
+++ templates/social-icons-links.php
@@ -1,22 +1,11 @@
-<?php
-$uri = $_SERVER['REQUEST_URI'];
-//echo $uri; // Outputs: URI
- 
-$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
- 
-$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
- // Outputs: Full URL
- 
-//$query = $_SERVER['QUERY_STRING'];
-//echo $query; // Outputs: Query String
-?>
 <div class="social-media">
     <!--Facebook button-->
-    <a target="_blank" class="fa fa-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url;?>"  data-layout="button" data-href="<?php echo $url;?>" ></a>
+    <a target="_blank" id="shareBtn" class="fa fa-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url;?>"  data-layout="button" data-href="<?php echo $url;?>" ></a>
     <!--Twitter button-->
-    <a target="_blank" class="fa fa-twitter" href="https://twitter.com/share?url=<?php echo $url;?>&amp;via=developerspotKE&amp;text=<?php echo $posts['post_title'] ?>&amp;hashtags=webdevelopment,webdesign" ></a>
+    <a target="_blank" class="fa fa-twitter" href="http://twitter.com/share?text=&url=<?php echo $url;?>&hashtags=webdevelopment,webdesign" >
+</a>
     <!--Pinterest button-->
-    <a target="_blank" class="fa fa-pinterest" href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonPin" data-pin-save="true" data-pin-custom="true"></a>
+    <a target="_blank" class="fa fa-pinterest" href="https://www.pinterest.com/pin/create/button/" data-pin-do="buttonPin" data-pin-save="" data-pin-custom="true"></a>
     <!--instagram button-->
-    <a target="_blank" class="fa fa-instagram" href="https://www.instagram.com/sharer/=<?php echo $url;?>&title=<?php echo $posts['post_title'] ?>&source=developerspot.co.ke" ></a>
+    <a target="_blank" class="fa fa-instagram" href="https://www.instagram.com/developerspotke" ></a>
 </div>
