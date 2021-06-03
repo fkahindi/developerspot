2a4d8dbf47ae1fd9bbfba68ee62d751e444725ee
Francis <fkahindi@gmail.com>
2021-06-03 08:56:45 +0300

    Redesign footer elements layout

--- templates/footer.html.php
+++ templates/footer.html.php
@@ -4,7 +4,7 @@
 <div class="policies">
 	<a href="<?php echo BASE_URL ?>policies/terms-conditions.php">Terms </a> <span>&#124;</span>
 	<a href="<?php echo BASE_URL ?>policies/privacy-policy.php">Privacy </a> <span>&#124;</span>
-	<a href="<?php echo BASE_URL ?>policies/cookie-policy.php">Cookie </a> <span>&#124;</span>
+	<a href="<?php echo BASE_URL ?>policies/cookie-policy.php">Cookie </a> 
 </div>
 <div class="nav flex-wrapper">
 	<?php include  __DIR__ . '/nav.html.php'; ?>	
