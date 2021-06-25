c80fe17f3177c6a3ac06ad684aeaa4084f2010ef
Francis <fkahindi@gmail.com>
2021-06-25 09:39:24 +0300

    Remove unsuported type value

--- templates/head-resources.html.php
+++ templates/head-resources.html.php
@@ -1,7 +1,7 @@
 <link rel="preload" href="<?php echo BASE_URL ?>resources/css/main.css" as="style">
 <link rel="preload" href="<?php echo BASE_URL ?>resources/font-awesome-4.7.0/css/font-awesome.css" as="style">
 <link rel="preload" href="<?php echo BASE_URL ?>resources/js/page-control.js" as="script">
-<link rel="preload" href="<?php echo BASE_URL ?>resources/fonts/Aladin-Regular.ttf" as="font" type="truetype" crossorigin>
+<link rel="preload" href="<?php echo BASE_URL ?>resources/fonts/Aladin-Regular.ttf" as="font" crossorigin>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/subscribe-comment-form.css" media="print" onload="this.media='all'; this.onload=null;"/>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/main.css" />
 <link rel="icon" href="<?php echo BASE_URL ?>resources/icons/devpot-favicon.png" sizes="16x16 32x32" type="image/x-icon"/>
