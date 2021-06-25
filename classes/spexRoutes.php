dec0ccd293b488b100ce78eb5635eeb0dbbe54a1
Francis <fkahindi@gmail.com>
2021-06-25 09:21:49 +0300

    Delete

--- classes/spexRoutes.php
+++
@@ -1,6 +0,0 @@
-<?php
-public function getAuthentication() {
-$membersTable = new \Frameworks\DatabaseTable($pdo,'members', 'email');
-return new \Frameworks\Authentication($authorsTable,
-'email', 'password');
-}
