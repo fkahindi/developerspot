a36025b32d9967e33cc6acf0d6fafc62a578d7c1
Francis <fkahindi@gmail.com>
2021-08-03 17:36:38 +0300

    Change favicon

--- templates/head-resources.html.php
+++ templates/head-resources.html.php
@@ -3,6 +3,6 @@
 <link rel="preload" href="<?php echo BASE_URL ?>resources/fonts/Aladin-Regular.ttf" as="font" crossorigin>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/subscribe-comment-form.css" media="print" onload="this.media='all'; this.onload=null;"/>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/main.css" />
-<link rel="icon" href="<?php echo BASE_URL ?>resources/icons/favicon.png" sizes="16x16 32x32" type="image/x-icon"/>
+<link rel="icon" href="<?php echo BASE_URL ?>resources/icons/logo_icon.png" sizes="16x16 32x32" type="image/x-icon"/>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/font-awesome-4.7.0/css/font-awesome.css" media="print" onload="this.media='all'; this.onload=null;"/>
 
