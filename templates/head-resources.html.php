a8c63f11e6d8cb6cdcad1c01c37e12c8533e5382
Francis <fkahindi@gmail.com>
2022-04-26 11:41:17 +0300

    remove preload link for Aladin font

--- templates/head-resources.html.php
+++ templates/head-resources.html.php
@@ -1,6 +1,5 @@
 <link rel="preload" href="<?php echo BASE_URL ?>resources/css/main.css" as="style">
 <link rel="preload" href="<?php echo BASE_URL ?>resources/font-awesome-4.7.0/css/font-awesome.css" as="style">
-<link rel="preload" href="<?php echo BASE_URL ?>resources/fonts/Aladin-Regular.ttf" as="font" crossorigin>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/subscribe-comment-form.css" media="print" onload="this.media='all'; this.onload=null;"/>
 <link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/main.css" />
 <link rel="icon" href="<?php echo BASE_URL ?>resources/icons/logoicon.png" sizes="16x16 32x32" type="image/x-icon"/>
