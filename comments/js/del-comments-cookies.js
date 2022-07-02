0c9242dad8ecb4282075947987e441febddb9fe3
Francis <fkahindi@gmail.com>
2022-07-02 14:53:49 +0300

    create comments-cookies script file

---
+++ comments/js/del-comments-cookies.js
@@ -0,0 +1,5 @@
+document.cookie = "commentCookie=; path=/; max-age=0";
+document.cookie = "pageId=; path=/; max-age=0";
+document.cookie = "userId=; path=/; max-age=0";
+document.cookie = "replyCookie=; path=/; max-age=0";
+document.cookie = "commentIdCookie=; path=/; max-age=0";
