b0fe28127cac0e1e73774ec7dfe537db62cdc1f7
Francis <fkahindi@gmail.com>
2022-07-02 14:29:34 +0300

    move base styles and facebook scripts to external files

--- about.php
+++ about.php
@@ -23,55 +23,12 @@ $thisPage = "ABOUT";
     <meta property="og:description" content="It's not just a blogging site but a blogging app, complete with an admin module for managing articles, authors and users. It has an embedded comment-reply system on all articles." />
     <meta property="og:image" content="<?php echo BASE_URL ?>resources/icons/logoicon.png" />
     <meta property="fb:app_id" content="502152493814762"/>
-	
-	<style>
-	html,body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-	}
-	article, aside, details, figcaption, figure,
-	footer, header, hgroup, menu, nav, section {
-	display: block;
-	}
-	.main-article {
-	margin: 1em auto;
-	padding: 1em 0.5em;
-	background-color: transparent; }
-	@media (max-width: 768px) {
-		.main-article {
-		width: 90%; } }
-	@media (max-width: 599px) {
-		.main-article {
-		width: 100%;
-		margin: 0 auto;
-		padding: 0.5em; } }
-	@media (min-width: 769px) {
-		.main-article {
-		width: 70%; } }
-	@media (min-width: 992px) {
-		.main-article {
-		width: 60%; } }
-
-	</style>	 
+		<link rel="preload" href="<?php echo BASE_URL ?>resources/css/about.css" as="style">
     <?php include_once __DIR__ .'/templates/head-resources.html.php'; ?>
+		<link rel="stylesheet" href="<?php echo BASE_URL ?>resources/css/about.css" />	 
 	
         <!-- Facebook Pixel Code -->
-        <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1970631019919003');
-        fbq('track', 'PageView');
-        </script>
+        <script src="resources/js/facebook-pixel.js"></script>
         
 </head>
 <body>
