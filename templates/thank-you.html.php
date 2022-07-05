0ef4e776749705111029692aaaab5b0832b9fdf3
Francis <fkahindi@gmail.com>
2022-07-05 12:41:52 +0300

    add css to thank-you page

--- templates/thank-you.html.php
+++ templates/thank-you.html.php
@@ -6,22 +6,29 @@ if(!isset($_SESSION)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Email | Message Success</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Email | Message Success</title>
+	<link rel="stylesheet" href="../resources/css/thank-you.css">
 </head>
 <body>
-<?php if(!empty($_SESSION['email_success'])):?>
-<div class="successMsg"><?php echo $_SESSION['email_success']; ?></div>
-<p> <a href="../login"> Continue</a></p>
-<?php unset($_SESSION['email_success']);?>
-<?php else: ?>
-<?php echo ''; endif; ?>
+	<?php if(!empty($_SESSION['email_success'])):?>
+		<div class="card">
+			<div class="success"><?php echo $_SESSION['email_success']; ?></div>
+			<p> <a href="../login"> Continue</a> ...</p>
+			<?php unset($_SESSION['email_success']);?>
+		</div>
+	<?php else: ?>
+	<?php echo ''; endif; ?>	
 
-<?php if(!empty($_SESSION['message_success'])):?>
-<p><?php echo($_SESSION['message_success']) ?> </p>
-<p> <a href="index.php"> Continue</a>.</p>
-<?php unset($_SESSION['message_success']);?>
-<?php else: ?>
-<?php echo ''; endif; ?>
-
+	<?php if(!empty($_SESSION['message_success'])):?>
+		<div class="card">
+			<p class="success"><?php echo($_SESSION['message_success']) ?> </p>
+			<p> <a href="index.php"> Continue</a> ...</p>
+			<?php unset($_SESSION['message_success']);?>
+		</div>
+	<?php else: ?>
+	<?php echo ''; endif; ?>
+	
 </body>
 </html>
