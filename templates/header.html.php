780f1de31e56caff2f3e880287f74eca0fb3f74b
Francis <fkahindi@gmail.com>
2022-08-08 15:01:42 +0300

    Add account settings and css

--- templates/header.html.php
+++ templates/header.html.php
@@ -20,7 +20,7 @@
 			<?php if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']== true):?>
 			<div class="flex-container">
 				<div class="account-photo-box tooltip ">
-					<span>  <img src="<?php echo $_SESSION['profile_photo'] ?>" alt="" width="30px" height="30px" data-pin-nopin="1" class="image-photo"></span></label>
+					<span> <img src="<?php echo $_SESSION['profile_photo'] ?>" alt="" width="30px" height="30px" data-pin-nopin="1" class="image-photo"></span></label>
 					<div class="tooltip-text">
 						<p><strong>Account:</strong></p>
 						<p><?php echo $_SESSION['fullname'] ?></p>
@@ -29,14 +29,13 @@
 				</div>
 				<div class="settings">
 					<label for="profile-checkbox-control" id="tooltip">
-					<span class="settings-char-icon"> &#133;</span>
+					<span class="settings-char-icon" title="Account settings"> &#133;</span>
 					<input type="checkbox" id="profile-checkbox-control">
 					<div class="account-display-settings">
 						<ul>
-							<li><a href="<?php echo BASE_URL ?>change-password">Change Password </a>
-							</li>
-							<li><a href="<?php echo BASE_URL ?>imageupload">Add Profile Photo</a></li>
 							<li><a href="<?php echo BASE_URL ?>includes/logout.php">Sign out </a></li>
+							<li><a href="<?php echo BASE_URL ?>imageupload">Add Profile Photo</a></li>
+							<li><a href="<?php echo BASE_URL ?>change-password">Change Password </a></li>
 							<?php if($_SESSION['role'] == 'Admin'):?>
 								<li><a href="<?php echo BASE_URL ?>admin/dashboard.php">Admin Area </a></li>
 							<?php elseif($_SESSION['role'] == 'Author'): ?>
